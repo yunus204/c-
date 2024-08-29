3c4d738e2150e6d1c149d9b67ae878cd002f7716
yunus204 <mdyunusmulla19@gmail.com>
2024-08-29 10:31:37 -0700

    Add files via upload

---
+++ Untitled1123.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+// create a class
+class room
+{
+public:
+  double length;
+  double breath;
+  double height;
+  double calculate_area()
+  {
+    return length * breath;
+  }
+  double calculate_volume()
+  {
+    return length * breath * height;
+  }
+};
+int main()
+{
+  // create object of room class
+  room room1;
+  // Assign values to daTA MEMEBER:
+  room1.length = 42.5;
+  room1.breath = 30.8;
+  room1.height = 19.2;
+  // Calculate and display the area and volume of the room:
+  cout << "Area of Room = " << room1.calculate_area() << endl;
+  cout << "Volume of Room = " << room1.calculate_volume() << endl;
+  return 0;
+}
